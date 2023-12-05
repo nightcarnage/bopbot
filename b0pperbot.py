604efaf38eea1b980b03404047c02319ad7dff45
wcbx <blackwc88@gmail.com>
2023-12-04 19:33:28 -0500

    update

--- b0pperbot.py
+++ b0pperbot.py
@@ -188,7 +188,7 @@ credit associated with each tipper.')
     if command == 'start':
         print('The "start" command enables song requests.')
     if command == 'stop':
-        print('The "stop" command disables song requests. Also disables credit command.')
+        print('The "stop" command disables song requests. It also disables credit command.')
 
 #if clean_playlist is specificed in config.ini
 #then when program is reset or exited it will
